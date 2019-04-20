391b41bf333aa69796cf0cadfe3786973d78fe0c
afeldmanx1 <alexf0922@gmail.com>
2019-04-20 17:42:27 -0400

    react router setup and settings page

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -5,10 +5,9 @@ export class index extends Component {
   render() {
     return (
       <div className="header">
-        <nav class="navbar navbar-dark bg-dark justify-content-center">
-          <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        <nav className="navbar-light bg-light p-2">
+          <form className="form-inline">
+            <input className="form-control text-center" type="search" placeholder="Search" aria-label="Search"></input>
           </form>
         </nav>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Login from './Components/Login';
+import Review from './Components/Review';
+import Settings from './Components/Settings'
+import Map from './Components/Map';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 import * as serviceWorker from './serviceWorker';
 
+const routing = (
+    <Router>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route exact path="/map" component={Map} />
+        <Route exact path="/footer" component={Footer} />
+        <Route exact path="/settings" component={Settings} />
+      </div>
+    </Router>
+  )
 
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
