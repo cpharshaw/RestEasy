ae775fb0bb59d9ebc86af86d70800fceeccc56fb
Craig <cpharshaw@users.noreply.github.com>
2019-04-23 20:09:40 -0400

    Craig fixes (#14)
    * working header footer and map layout
    
    * playing with the map

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Header from './Components/Header';
 import MapWithASearchBox from './Components/MapWithASearchBox';
 import Review from './Components/Review';
 import Footer from './Components/Footer';
-
 import Settings from './Components/Settings'
 import Button from 'react-bootstrap/Button';
 
