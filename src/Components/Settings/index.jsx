05a16f862d0be48698e914cfe6f43aa4acd87dae
afeldmanx1 <alexf0922@gmail.com>
2019-04-29 22:23:36 -0400

    layout styling changes

--- src/Components/Settings/index.jsx
+++ src/Components/Settings/index.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import './settings.css';
 import Footer from '../Footer';
-import $ from 'jquery';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
+// import $ from 'jquery';
+// import Button from 'react-bootstrap/Button';
+// import Collapse from 'react-bootstrap/Collapse';
 import MapWithASearchBox from '../MapWithASearchBox';
 
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
@@ -34,17 +34,14 @@ export class Settings extends Component {
             </p>
           </div>
 
-          <br />
-          <br />
-
           <img src="./user-settings.png" alt="Logo" className="user-image rounded-circle border border-dark"></img>
 
+          <br />
           <br />
           <p className="text-center">{this.props.user.displayName}</p>
           <br />
 
           <Link className="home-button nav-item nav-link" to='/MapWithASearchBox'><button type="button" className="btn btn-secondary font-weight-bold text-body btn-lg btn-block">Back to Map</button></Link>
-          <Link className="home-button nav-item nav-link" to='#'><button type="button" className="btn btn-secondary font-weight-bold text-body btn-lg btn-block">About Us</button></Link>
           <Link className="home-button nav-item nav-link" to='/'><button type="button" className="btn btn-secondary font-weight-bold text-body btn-lg btn-block" onClick={this.props.signOut}>Logout</button></Link>
 
 
