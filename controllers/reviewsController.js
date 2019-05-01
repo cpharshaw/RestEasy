400b7d7510a280c8f21e37f499bd26867e0e1306
cpharshaw <cpharshaw+git@gmail.com>
2019-05-01 06:41:45 -0400

    base aggregate query from mongo works

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -7,12 +7,38 @@ router.use(express.json());
 // Defining methods for the reviewsController
 module.exports = {
   findAll: function(req, res) {
-    db.Review
-      .find(req.query)
-      .sort({ date: -1 })
-      .then(dbModel => res.json(dbModel))
+    console.log(req.query);
+      
+    // db.Review.find({})
+      // .sort({ date: -1 })
+      db.Review.aggregate([
+        {
+          $group: {
+            _id: {
+              place_id: "$place_id",
+              position: "$position",
+              avgOverall: "$avgOverall"
+            },
+            // position: "position",
+            avgClean: {$avg: "$review_cleanliness"},
+            avgCap: {$avg: "$review_capacity"},
+            avgStyle: {$avg: "$review_style"},
+            avgOverall: {$avg: { $avg: ["$review_cleanliness", "$review_capacity", "$review_style"]}}
+          },
+       }
+      ])
+      .then(dbModel => console.log(dbModel))
+      // .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findAllAggregate: function(req, res) {
+    console.log(req.query);
+    // db.Review
+    //   .find(req.query)
+    //   .sort({ date: -1 })
+    //   .then(dbModel => res.json(dbModel))
+    //   .catch(err => res.status(422).json(err));
+  },  
   findById: function(req, res) {
     db.Review
       .findById(req.params.id)
